ec41d65554c526aea0dd8061a9845a818f0b7186
DusanSekulic9 <dukiseki@gmail.com>
2021-01-07 17:56:02 +0100

    models,forms, admin

--- skriptProjekat2/telefoni/models.py
+++ skriptProjekat2/telefoni/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import Userg
+from django.contrib.auth.models import User
 # Create your models here.
 class Article(models.Model):
     marka = models.CharField(max_length=32,default='')
