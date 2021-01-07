67d533a17b505660d91753a949414ca35172d97e
DusanSekulic9 <dukiseki@gmail.com>
2021-01-07 17:54:35 +0100

    models,forms, admin

--- skriptProjekat2/telefoni/models.py
+++ skriptProjekat2/telefoni/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from django.contrib.auth.models import Userg
 # Create your models here.
+class Article(models.Model):
+    marka = models.CharField(max_length=32,default='')
+    naziv = models.CharField(max_length=32,default='')
+    sistem = models.CharField(max_length=32, default='')
+    num_views = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.marka + ' ' + self.naziv
+
+
+class Comment(models.Model):
+    content = models.CharField(max_length=64)
+    num_views = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.content
